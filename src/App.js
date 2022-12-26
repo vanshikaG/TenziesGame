d8233899912716ebb9bcc47c210b11eb543105a4
vanshikaG <vanshikagautam22@gmail.com>
2022-12-26 19:46:14 +0530

    Roll dice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Die from "./Die"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
 
-export default App;
+      const [dice, setDice] = React.useState(allNewDice())
+      
+      function allNewDice() {
+          const newDice = []
+          for (let i = 0; i < 10; i++) {
+              newDice.push(Math.ceil(Math.random() * 6))
+          }
+          return newDice
+      }
+      
+      function rollDice() {
+          setDice(allNewDice())
+      }
+      
+      const diceElements = dice.map(die => <Die value={die} />)
+      
+      return (
+          <main>
+              <div className="dice-container">
+                  {diceElements}
+              </div>
+              <button className="roll-dice" onClick={rollDice}>Roll</button>
+          </main>
+      )
+  }
